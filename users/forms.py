0ce220df7d52ed859e709b33ff6ff5135cf55301
dogaozyagci <75997342+dogaozyagci@users.noreply.github.com>
2022-04-30 21:39:22 +0300

    style added

--- users/forms.py
+++ users/forms.py
@@ -18,14 +18,14 @@ class BusinessUserCreationForm(UserCreationForm):
         fields = ['email', 'password1', 'password2', 'business_name',
                   'business_type', 'tax_id', 'phone', 'address', 'information']
         widgets = {
-            'email': forms.TextInput(attrs={'class': 'form-control'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'business_name': forms.TextInput(attrs={'class': 'form-control'}),
-            'business_type': forms.Select(attrs={'class': 'form-select', 'style': "width:250px"}),
-            'tax_id': forms.TextInput(attrs={'class': 'form-control'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '( )___ ___ __ __'}),
-            'address': forms.Textarea(attrs={'class': 'form-control', 'style': "background:#f6f6f6"}),
-            'information': forms.Textarea(attrs={'class': 'form-control', 'style': "background:#f6f6f6"}),
+            'email': forms.TextInput(attrs={'class': 'unicorn'}),
+            'password1': forms.PasswordInput(attrs={'class': 'unicorn'}),
+            'password2': forms.PasswordInput(attrs={'class': 'unicorn'}),
+            'business_name': forms.TextInput(attrs={'class': 'unicorn'}),
+            'business_type': forms.Select(attrs={'class': 'unicorn', 'style': "width:340px"}),
+            'tax_id': forms.TextInput(attrs={'class': 'unicorn'}),
+            'phone': forms.TextInput(attrs={'class': 'unicorn', 'placeholder': '( )___ ___ __ __'}),
+            'address': forms.Textarea(attrs={'class': 'unicorn', 'style': "background:#f6f6f6"}),
+            'information': forms.Textarea(attrs={'class': 'unicorn', 'style': "background:#f6f6f6"}),
         }
 
