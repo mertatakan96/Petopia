9eb20fbfa29403aecdb95a2e9d5d2906615b5c31
Mert Atakan Duduoğlu <atakanduduoglu@gmail.com>
2022-05-15 19:16:24 +0300

    forum models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Forum(models.Model):
+    owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    forum_image = models.ImageField(null=True, blank=True, upload_to='forums/', default="dog1.jpg")
+    title = models.CharField(max_length=200, null=True)
+    post = models.TextField(max_length=750, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.title) + " - " + str(self.owner)
+
+    class Meta:
+        ordering = ['-created']
+
+class ForumComment(models.Model):
+    owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    forum = models.ForeignKey(Forum, null=True, on_delete=models.CASCADE)
+    body = models.TextField(max_length=120, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.forum) + " - " + str(self.owner)
+
+    class Meta:
+        ordering = ['created']
